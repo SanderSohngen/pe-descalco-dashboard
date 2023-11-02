39af7c78703f61ea3c3c85b2ad9b65d969093be2
Sander Sohngen <sander.sohngen@usp.br>
2023-11-02 14:02:48 -0300

    created template for all apps

--- pd_pin/settings.py
+++ pd_pin/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'class_council',
+    'classroom',
+    'graduation',
+    'songs',
+    'users',
 ]
 
 MIDDLEWARE = [
