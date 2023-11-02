2ae961e6a7cbaf5ebb12445632c4716c01d0ee88
Sander Sohngen <sander.sohngen@usp.br>
2023-11-02 14:19:05 -0300

    implemented graduation models

--- graduation/models.py
+++ graduation/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Graduation(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Criteria(models.Model):
+    graduation = models.ForeignKey(
+        Graduation,
+        related_name="criterias",
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
