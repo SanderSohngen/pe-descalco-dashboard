a636f058d539119f9668370283ec5816e27e790e
Sander Sohngen <sander.sohngen@usp.br>
2023-11-03 21:16:33 -0300

    improved users model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-from graduation.models import Graduation
 from classroom.models import Classroom
+from graduation.models import Graduation
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
 
 
 class Role(models.TextChoices):
@@ -11,15 +16,19 @@ class Role(models.TextChoices):
 
 
 class Student(models.Model):
+    user = models.ForeignKey(
+        User, related_name='students', on_delete=models.CASCADE)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     graduation = models.ForeignKey(Graduation, on_delete=models.CASCADE)
     classrooms = models.ManyToManyField(Classroom, related_name='students')
 
     def __str__(self):
-        return f"{self.role} {self.graduation}"
+        return f"{self.user.get_full_name}, {self.role}-{self.graduation}"
 
 
 class Teacher(models.Model):
+    user = models.ForeignKey(
+        User, related_name='teachers', on_delete=models.CASCADE)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     classrooms = models.ManyToManyField(Classroom, related_name='teachers')
 
@@ -28,12 +37,4 @@ class Teacher(models.Model):
             f"{classroom.graduation} - {classroom.day}"
             for classroom in self.classrooms.all()
         ])
-        return f"{self.role} {classroom_info}"
-
-
-class User(AbstractUser):
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.EmailField()
-    student = models.ManyToManyField(Student, related_name='users')
-    teacher = models.ManyToManyField(Teacher, related_name='users', blank=True)
+        return f"{self.user.get_full_name}: {self.role} {classroom_info}"
