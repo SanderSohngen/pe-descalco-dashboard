9647a015d169f94e21f0bfacefa7c50bce2aa943
Sander Sohngen <sander.sohngen@usp.br>
2023-11-03 22:45:54 -0300

    implemented attendance models

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from users.models import Student, Teacher
+from classroom.models import ClassroomInstance
 
-# Create your models here.
+
+class Attendance(models.Model):
+    classroom_instance = models.ForeignKey(
+        ClassroomInstance,
+        related_name='attendances',
+        on_delete=models.CASCADE
+    )
+    attended = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class StudentAttendance(Attendance):
+    student = models.ForeignKey(
+        Student, related_name='attendances', on_delete=models.CASCADE)
+
+    def __str__(self):
+        attended_str = 'Presente' if self.attended else 'Ausente'
+        return f"{self.student} - {self.classroom_instance}, {attended_str}"
+
+
+class TeacherAttendance(Attendance):
+    teacher = models.ForeignKey(
+        Teacher, related_name='attendances', on_delete=models.CASCADE)
+
+    def __str__(self):
+        attended_str = 'Presente' if self.attended else 'Ausente'
+        return f"{self.teacher} - {self.classroom_instance}, {attended_str}"
