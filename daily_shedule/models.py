b6b047505fa62075595618baaa2e0fc3f7d9b4bf
Sander Sohngen <sander.sohngen@usp.br>
2023-11-03 21:37:22 -0300

    implemented daily schedule models

--- daily_shedule/models.py
+++ daily_shedule/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Day(models.Model):
+    date = models.DateField(unique=True)
+
+    def __str__(self):
+        return self.date.strftime('%d/%m/%y')
